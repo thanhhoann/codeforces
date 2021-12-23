90b704fe0284de667af0213583b4db6304bf6b8c
thanhhoan <pdth456@gmail.com>
2021-12-23 12:10:17 +0700

    add codeforces/265A.cpp at 12/23/2021, 12:10:17.

---
+++ 265A.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#define ll long long
+
+#define db(x) cout << #x << " = " << x << endl;
+
+void solve() {
+  string s, t;
+  cin >> s >> t;
+
+  int count = 0;
+
+  for (int i = 0; i < t.length(); i++) {
+    if (s[count] == t[i])
+      count++;
+  }
+  cout << count + 1;
+}
+
+int main() {
+  solve();
+  return 0;
+}
