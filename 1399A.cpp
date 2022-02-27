25251f07b3752d87f1600f60131eed18b0bd7c75
thanhhoan <pdth456@gmail.com>
2022-02-27 19:37:36 +0700

    complete 27/2

---
+++ 1399A.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#define ll long long
+
+#define db(x) cout << #x << " = " << x << endl;
+
+void solve() {
+  int n, num;
+  for (int i = 0; i < n; i++) {
+    cin >> num;
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  cout.tie(0);
+
+  int test_cases;
+  cin >> test_cases;
+  while (test_cases--) {
+    solve();
+  }
+
+  return 0;
+}
